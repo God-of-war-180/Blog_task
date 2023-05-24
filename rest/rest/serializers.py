3b5fab9e404ecc08828d5c8d18447b1aaf804089
God-of-war-180 <121376159+God-of-war-180@users.noreply.github.com>
2023-05-24 12:51:38 +0530

    Update serializers.py

--- rest/rest/serializers.py
+++ rest/rest/serializers.py
@@ -1,6 +1,6 @@
 from djoser.serializers import UserCreateSerializer
 
-
+#we created this class in order to customize getting user credentials in order to create a user profile
 class UserSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         fields = ["id","username","email","password"]
